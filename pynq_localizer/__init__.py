58272630b4e3154b5b40a5229ff5d0958ca57296
SiririComun <pablo.sanchez2@udea.edu.co>
2026-08-23 10:52:16 -0500

    feat(kinematics): implement KinematicAnalytics DSP engine with sub-Hertz STFT ridge tracker

--- pynq_localizer/__init__.py
+++ pynq_localizer/__init__.py
@@ -5,6 +5,7 @@ pynq_sound_localizer: FPGA-Accelerated Acoustic Kinematics, Doppler Tracking & S
 from pynq_localizer.loader import HardwareLoader
 from pynq_localizer.hw_trigger import HardwareTrigger
 from pynq_localizer.array import MicrophoneArrayOverlay
+from pynq_localizer.kinematics import KinematicAnalytics
 from pynq_localizer.notebooks import copy_notebooks
 
 __version__ = "1.0.0"
@@ -12,5 +13,6 @@ __all__ = [
     "HardwareLoader",
     "HardwareTrigger",
     "MicrophoneArrayOverlay",
+    "KinematicAnalytics",
     "copy_notebooks",
 ]
