540c19a2f84a0d37970c3e6ea3a3c7b9a48ada04
SiririComun <pablo.sanchez2@udea.edu.co>
2026-08-23 10:44:59 -0500

    feat(array): implement MicrophoneArrayOverlay with zero-skew capture and continuous flight recorder

--- pynq_localizer/__init__.py
+++ pynq_localizer/__init__.py
@@ -4,11 +4,13 @@ pynq_sound_localizer: FPGA-Accelerated Acoustic Kinematics, Doppler Tracking & S
 
 from pynq_localizer.loader import HardwareLoader
 from pynq_localizer.hw_trigger import HardwareTrigger
+from pynq_localizer.array import MicrophoneArrayOverlay
 from pynq_localizer.notebooks import copy_notebooks
 
 __version__ = "1.0.0"
 __all__ = [
     "HardwareLoader",
     "HardwareTrigger",
+    "MicrophoneArrayOverlay",
     "copy_notebooks",
 ]
